e0e374695289286404be7c993ac546d50753df39
thunderblade23 <113013471+thunderblade23@users.noreply.github.com>
2025-02-17 14:47:49 +0530

    Add files via upload

---
+++ Preporatory - Day 1/Demo.java
@@ -0,0 +1,5 @@
+class Demo1{
+	public static void main(String args[]){
+		System.out.println("Hello PG-DAC Feb-25...!!!, CDAC Mumbai...!!!");
+	}
+}
---
+++ Preporatory - Day 1/Test.java
@@ -0,0 +1,38 @@
+class Test{
+	public static void main(String args[]){
+
+/*	int a = 10; 
+
+	System.out.println("Value of a = " + a); //To concatenate with String
+	System.out.println(+ a); //sop
+	System.out.println(a + a);
+
+	byte a = (byte)1341230;
+	System.out.println(+ a);
+
+	int a = 10; // define & declaration
+	int a1;     // define
+
+	short s = a; // Error because bigger value data type is stored in smaller value data type
+	System.out.println(s);
+	
+	int a = 10;
+	float f = a;
+	System.out.println(f); // Saves as a float value
+
+	float a = 10.0f;
+	int f = a; // possible lossy conversion
+	System.out.println(f);
+
+	float a = 10.0f;
+	int f = (int)a;
+	System.out.println(f);
+
+	float f1 = 10.0;
+	System.out.println(f1);
+
+	double f1 = 10.0;
+	System.out.println(f1);
+*/
+	}
+}
