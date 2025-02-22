8ed434bc94e366029bc949e5c6dc58d4221332c2
thunderblade23 <113013471+thunderblade23@users.noreply.github.com>
2025-02-22 21:15:18 +0530

    Delete Preporatory - Day 1 directory

--- Preporatory - Day 1/Demo.java
+++
@@ -1,5 +0,0 @@
-class Demo1{
-	public static void main(String args[]){
-		System.out.println("Hello PG-DAC Feb-25...!!!, CDAC Mumbai...!!!");
-	}
-}
--- Preporatory - Day 1/Name.java
+++
@@ -1,5 +0,0 @@
-class Name{
-	public static void main(String args[]){
-		System.out.println("Hello\n\nPeeyush Kothekar");
-	}
-}
